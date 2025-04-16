da608d38eabd6db4b11a55a664c75b607f0b8cf2
MarwaneGP <marwane.ghalila@hotmail.com>
2025-04-16 14:28:59 +0200

    AppContainre

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,14 @@
 import { View, TextInput } from 'react-native';
 import { AppProvider } from './context';
+import AppContainer from '../components/AppContainer';
 export default function TextGetter() {
 
   return (
     <AppProvider>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '80%' }}
-          placeholder="Type here..."
-        />
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '80%', marginTop: 20 }}
-          placeholder="Type here..."
-        />
-      </View>
+      <AppContainer childElement={ }>
+
+      </AppContainer>
+
     </AppProvider>
   );
 }
